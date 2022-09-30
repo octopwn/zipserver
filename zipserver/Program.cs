d0f10a7e71959443c6c5b026689496eae3711b2a
skelsec <info@skelsec.com>
2022-09-30 10:26:18 -0700

    initial commit

---
+++ zipserver/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using BasicHTTPServer;
+
+namespace zipserver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length < 1)
+            {
+                Console.WriteLine("Args: zipfilepath, address, port");
+                return;
+            }
+            string zipfilepath = args[0];
+            string address = "127.0.0.1";
+            int port = 8000;
+            if (args.Length > 1)
+            {
+                address = args[1]; 
+            }
+            if (args.Length > 2)
+            {
+                port=int.Parse(args[2]);
+            }
+            Console.WriteLine("http://" + address + ":" + port.ToString());
+            SimpleHTTPServer server = new SimpleHTTPServer(zipfilepath, address:address, port:port);
+        }
+    }
+}
